3208387f14c78c25ea8152f8b9379366460669f4
RomeshAbeyawardena <romesh.abeyawardena@concerto.co.uk>
2021-05-14 17:48:11 +0100

    - renamed powershell script

---
+++ dotnet-install-solution.ps1
@@ -0,0 +1,7 @@
+﻿ param (
+    $projectName,
+    $solutionRoot,
+    $projects
+ )
+ 
+ Invoke-Expression "$PSScriptRoot\src\bin\PackageManager.Console.exe -o $solutionRoot -s $projectName -n $projects" 
--- run.ps1
+++
@@ -1,7 +0,0 @@
-﻿ param (
-    $projectName,
-    $solutionRoot,
-    $projects
- )
- 
- .\src\bin\PackageManager.Console.exe -o $solutionRoot -s $projectName -n $projects
